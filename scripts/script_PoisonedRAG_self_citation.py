9ee42527035c8fe2fd0fe5777ccd389463293a94
WYT8506 <dongshanbo123@gmail.com>
2025-08-06 22:16:04 -0400

    update

--- scripts/script_PoisonedRAG_self_citation.py
+++ scripts/script_PoisonedRAG_self_citation.py
@@ -1,6 +1,8 @@
 import os
 attr_type = 'self_citation' # 'tracllm', 'vanilla_perturb' or 'self_citation'
-dataset_name= 'nq-poison' #choose from 'nq-poison','hotpotqa-poison', and 'msmarco-poison'
+dataset_name= 'nq-poison' #choose from 'musique','narrativeqa','qmsum', 'nq-poison', 'hotpotqa-poison', and 'msmarco-poison'
+prompt_injection_attack = 'default' 
+inject_times = 5 # number of injected instructions
 model_name = "llama3.1-8b"
 data_num = 100 # number of evaluation data points
 explanation_level = "segment" # 'sentence','paragraph' or 'segment'
@@ -15,9 +17,11 @@ cmd = f'nohup python -u main.py \
 --attr_type {attr_type} \
 --K {K} \
 --explanation_level {explanation_level} \
+--prompt_injection_attack {prompt_injection_attack} \
+--inject_times {inject_times} \
 --model_name {model_name} \
 --gpu_id {gpu_id} \
 --verbose 0 \
 --data_num {data_num} \
-> ./out/{dataset_name}_{model_name}_{attr_type}_{K}.out &'
+> ./out/{dataset_name}_{model_name}_{prompt_injection_attack}_{inject_times}_{attr_type}_{K}.out &'
 os.system(cmd)
